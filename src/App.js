e8edf950e1de21d3d5dba681143929946e809f5c
KimHS0915 <remember2552@gmail.com>
2020-09-27 23:50:53 +0900

    Add React Component Counter and CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Counter from "Counter";
+import Message from "Message";
+import Profile from "Profile";
 import "App.css";
 
 class App extends React.Component {
-  state = { myquery : "", language: "" }
-  onChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    }); 
-  }
-
   render() {
     return (
-      <>
-        <Counter onClick={() => console.log("clicked")}/>
-        <input name="myquery" onChange={this.onChange} />
-        <input name="language" onChange={this.onChange} />
-        <hr/>
-        {JSON.stringify(this.state)}
-      </>
+      <div>
+        <Profile />
+        <Message />
+        <Counter />
+        <Counter color="green" />
+        <Counter color="blue" />
+      </div>
     ); 
   }
 }
