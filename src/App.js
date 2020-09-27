3abc9cbefd1dcbc070a570602df633600d099e54
KimHS0915 <remember2552@gmail.com>
2020-09-27 21:35:54 +0900

    React Element, State

--- src/App.js
+++ src/App.js
@@ -2,15 +2,56 @@ import React from "react";
 import { Button } from 'antd';
 import "App.css";
 
+const actions = {
+  init(initialValue) {
+    return { value: initialValue }
+  },
+  increment(prevState) {
+    return { value: prevState.value + 1 }
+  },
+  decrement(prevState) {
+    return { value: prevState.value - 1 }
+  }
+};
+
+class Counter1 extends React.Component {
+  state = actions.init(this.props.initialValue);
+
+  render() {
+    const { value } = this.state;
+    return (
+      <div>
+        Counter1: {value}
+        <Button onClick={() => this.setState(actions.increment)}>+1</Button>
+        <Button onClick={() => this.setState(actions.decrement)}>-1</Button>
+      </div>
+    );
+  }
+}
+
+class FruitComponent extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>fruits</h1>
+        <ul>
+          {
+            this.props.fruits.map((name, index) => (
+              <li key={index}>{name}</li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
+}
+
 function App() {
+  const fruits = ["apple", "mango", "pineapple"];
   return (
     <div>
-      <button>
-        Hello, React!
-      </button>
-      <Button type='primary' onClick={ () => console.log("clicked")}>
-        Hello, Antd!
-      </Button>
+      <Counter1 initialValue={10} />
+      <FruitComponent fruits={fruits} />
     </div>
   );
 }
