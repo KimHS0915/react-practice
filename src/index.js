8f50b837885eceafd38f634501ce5b2ecb62084e
KimHS0915 <remember2552@gmail.com>
2020-09-29 23:59:27 +0900

    Context API

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import './index.css';
 // import App3 from './App3';
 // import App4 from "App4";
 // import App5 from "App5";
-import App6 from "App6";
+// import App6 from "App6";
+// import App7 from "App7";
+// import App8 from "App8";
+import App9 from "App9";
 // import TodoList from "./TodoList";
 // import EpisodeList from "EpisodeList";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App6 />, document.getElementById('root'));
+ReactDOM.render(<App9 />, document.getElementById('root'));
 // ReactDOM.render(<TodoList />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
